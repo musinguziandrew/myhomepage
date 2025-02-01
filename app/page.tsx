c3c79609bdf129df0b4afe5ca566c46a50b05ddd
musinguziandrew <154724727+musinguziandrew@users.noreply.github.com>
2025-02-01 18:39:12 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ export default function Page() {
         Musinguzi Andrew 
       </h1>
       <p className="mb-4">
-        {`Mr. Musinguzi Andrew is a Physics researcher specilizing Non Linear Optics currently persuing his Masters Degree at University of Hyderabad. 
-        His work focuses on studying how light interacts with matter.His recent project is focusing on study the atmosphere using lasers by mimicking 
+        {`Mr. Musinguzi Andrew is a Physics researcher specilizing Non Linear Optics. He is currently persuing his Masters Degree at University of Hyderabad. 
+        His work focuses on studying how light interacts with matter. His recent project is focusing on studying the atmosphere using lasers by mimicking 
         atmospheric conditions in the laboratory using aerosols of different refractive index and properties revealing the effect of matter on light and
-        vice versa, with applications in the long range space communications using Ridars and Radars. Mr. Andrew has received many Awards and scholarships 
-        and actively engage in conferences and Teaching.`}
+        vice versa, with applications in the long range space communications using Ridars and Radars. Mr. Andrew has received many Awards and scholarships
+        and he actively engage in conferences and Teaching.`}
       </p>
       <div className="my-8">
         <BlogPosts />
