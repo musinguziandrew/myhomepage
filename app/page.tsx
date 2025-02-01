fec8ceb99ec6704143a0ddb56ab23ef4a0f5a790
musinguziandrew <154724727+musinguziandrew@users.noreply.github.com>
2025-02-01 17:57:33 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
         Musinguzi Andrew 
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
+        {`I'm a physics enthusiast and tab advocate, finding unmatched efficiency in
         Vim's keystroke commands and tabs' flexibility for personal viewing
         preferences. This extends to my support for static typing, where its
         early error detection ensures cleaner code, and my preference for dark
