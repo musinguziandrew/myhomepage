dfb8347dcd03d3c8804701df699311869d0bcbcb
musinguziandrew <154724727+musinguziandrew@users.noreply.github.com>
2025-02-01 18:31:21 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ export default function Page() {
         Musinguzi Andrew 
       </h1>
       <p className="mb-4">
-        {`I'm a physics enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`Mr. Musinguzi Andrew is a Physics researcher specilizing Non Linear Optics currently persuing his Masters Degree at University of Hyderabad. 
+        His work focuses on studying how light interacts with matter.His recent project is focusing on study the atmosphere using lasers by mimicking 
+        atmospheric conditions in the laboratory using aerosols of different refractive index and properties revealing the effect of matter on light and
+        vice versa, with applications in the long range space communications using Ridars and Radars. Mr. Andrew has received many Awards and scholarships 
+        and actively engage in conferences and Teaching.`}
       </p>
       <div className="my-8">
         <BlogPosts />
